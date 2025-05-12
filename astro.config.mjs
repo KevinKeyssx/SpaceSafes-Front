7a4c76c2f4b6b58d44cbf07477b2441c62e13d7f
kevinkeyssx <keyss1700@gmail.com>
2025-05-12 01:45:18 -0400

    Astro config added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
+import { esES } from "@clerk/localizations";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        tailwind(),
+        svelte(),
+        clerk({
+            localization: esES
+        })
+    ],
+    adapter: node({ mode: "standalone" }),
+    output: "server",
+});
