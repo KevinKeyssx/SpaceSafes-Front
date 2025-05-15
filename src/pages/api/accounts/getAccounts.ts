ab518a93dfb67c303c02da9af3f91720c6c6013a
kevinkeyssx <keyss1700@gmail.com>
2025-05-15 05:29:07 -0400

    Get account in services added

---
+++ src/pages/api/accounts/getAccounts.ts
@@ -0,0 +1,20 @@
+import type { APIRoute, APIContext } from 'astro';
+
+export const GET: APIRoute = async (context: APIContext) => {
+    const userId = context.locals.userId;
+    const token  = context.locals.authToken;
+
+    const response = await fetch(`http://localhost:3017/api/v1/accounts/users/${userId}`, {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        },
+    });
+
+    const accounts = await response.json();
+
+    return new Response(JSON.stringify(accounts), {
+        headers: {
+        'Content-Type': 'application/json',
+        },
+    });
+};
