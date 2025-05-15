8987902ca5c9168998e62a20f83c44c4bcb3c1d8
kevinkeyssx <keyss1700@gmail.com>
2025-05-15 05:26:36 -0400

    UserId & token added in context

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,15 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
 const matcher = createRouteMatcher( ["/dashboard(.*)"] );
 
-export const onRequest = clerkMiddleware(( auth, context ) => {
-    const { userId, redirectToSignIn } = auth();
+export const onRequest = clerkMiddleware(async ( auth, context ) => {
+    const { userId, redirectToSignIn, getToken } = auth();
 
     if ( matcher( context.request ) && !userId ) {
         return redirectToSignIn();
     }
 
+    const token = await getToken();
+
+    context.locals.userId       = userId;
+    context.locals.authToken    = token;
 });
