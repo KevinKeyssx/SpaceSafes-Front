3bfd9e7f37f5cf0c0f1a6925c3c05c82fde3ffcf
kevinkeyssx <keyss1700@gmail.com>
2025-05-16 06:01:15 -0400

    Type card added

---
+++ src/models/balance/enum/type-card.enum.ts
@@ -0,0 +1,14 @@
+export enum TypeCard {
+    VISA                = 'VISA',
+    VISA_ELECTRON       = 'VISA_ELECTRON',
+    MASTERCARD          = 'MASTERCARD',
+    AMERICAN_EXPRESS    = 'AMERICAN_EXPRESS',
+    DISCOVER            = 'DISCOVER',
+    JCB                 = 'JCB',
+    DINERS_CLUB         = 'DINERS_CLUB',
+    UNIONPAY            = 'UNIONPAY',
+    MAESTRO             = 'MAESTRO',
+    ELO                 = 'ELO',
+    HIPERCARD           = 'HIPERCARD',
+    AURA                = 'AURA'
+}
