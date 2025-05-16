b06fa94a73a29287e915c40a39f86425151da7c6
kevinkeyssx <keyss1700@gmail.com>
2025-05-16 06:01:07 -0400

    type balance enum added

---
+++ src/models/balance/enum/type-balance.enum.ts
@@ -0,0 +1,10 @@
+export enum TypeBalance {
+    CREDIT          = 'CREDIT',
+    DEBIT           = 'DEBIT',
+    BANK_ACCOUNT    = 'BANK_ACCOUNT',
+    CASH            = 'CASH',
+    FREELANCE       = 'FREELANCE',
+    SAVINGS         = 'SAVINGS',
+    INVESTMENT      = 'INVESTMENT',
+    OTHER           = 'OTHER'
+}
