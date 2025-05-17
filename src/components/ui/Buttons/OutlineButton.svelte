667a9e4ae235fe7cd01297588f08903bf50d67bf
kevinkeyssx <keyss1700@gmail.com>
2025-05-17 09:11:11 -0400

    OutlineButton added

---
+++ src/components/ui/Buttons/OutlineButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+    export let onClick: () => void;
+
+</script>
+
+<button
+type="button"
+on:click={() => onClick()}
+class="mb-0.5 w-9 h-9 flex items-center justify-center py-2 ring-1 ring-primary-400 hover:bg-primary-700 text-white rounded-lg transition-colors"
+aria-label="Habilitar descripción"
+>
+<slot />
+</button>
