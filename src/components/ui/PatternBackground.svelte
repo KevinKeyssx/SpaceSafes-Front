b199b5a6dcfc5fbe5787d3ae45904488cc7067e5
kevinkeyssx <keyss1700@gmail.com>
2025-05-17 09:10:49 -0400

    Bug fixed in opacity

--- src/components/ui/PatternBackground.svelte
+++ src/components/ui/PatternBackground.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
-    // Propiedades para personalizar el patrón
-    export let opacity: number = 5; // Opacidad por defecto (en porcentaje)
-    export let strokeColor: string = "white"; // Color del trazo
-    export let strokeWidth: number = 0.5; // Ancho del trazo
-    export let gridSize: number = 10; // Tamaño de la cuadrícula
-    export let patternId: string = "smallGrid"; // ID único para el patrón
+    export let strokeColor  : string = "white"; // Color del trazo
+    export let strokeWidth  : number = 0.5; // Ancho del trazo
+    export let gridSize     : number = 10; // Tamaño de la cuadrícula
+    export let patternId    : string = "smallGrid"; // ID único para el patrón
 </script>
 
-<div class="absolute inset-0 opacity-{opacity}">
+<div class="absolute inset-0 opacity-5">
     <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
         <defs>
             <pattern id="{patternId}" width="{gridSize}" height="{gridSize}" patternUnits="userSpaceOnUse">
