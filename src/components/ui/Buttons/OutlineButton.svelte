603b0c433d0dda67222db0b2715aa88bb2aff123
kevinkeyssx <keyss1700@gmail.com>
2025-05-20 09:37:30 -0400

    Outline button updated

--- src/components/ui/Buttons/OutlineButton.svelte
+++ src/components/ui/Buttons/OutlineButton.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
-
     export let onClick: () => void;
+    export let ariaLabel: string = "aria-label";
+    export let classProp: string = "";
 
+    const baseClasses = "mb-0.5 w-9 h-9 flex items-center justify-center py-2 ring-1 ring-primary-400 hover:bg-primary-700 text-white rounded-lg transition-colors";
 </script>
 
 <button
-type="button"
-on:click={() => onClick()}
-class="mb-0.5 w-9 h-9 flex items-center justify-center py-2 ring-1 ring-primary-400 hover:bg-primary-700 text-white rounded-lg transition-colors"
-aria-label="Habilitar descripción"
+    type="button"
+    on:click={() => onClick()}
+    class={`${baseClasses} ${classProp}`.trim()}
+    aria-label={ariaLabel}
 >
-<slot />
+    <slot />
 </button>
