f1dcd9139ff3b8fb29624c6ab8ac2450e67f1357
kevinkeyssx <keyss1700@gmail.com>
2025-05-20 09:41:12 -0400

    Import phpsphor updated

--- src/lib/balances/get-balance-icon.ts
+++ src/lib/balances/get-balance-icon.ts
@@ -1,21 +1,22 @@
 import { TypeBalance }  from '@/models/balance/enum/type-balance.enum';
 
-import CreditCardIcon   from "phosphor-svelte/lib/CreditCard";
-import BankIcon         from "phosphor-svelte/lib/Bank";
-import CashIcon         from "phosphor-svelte/lib/Money";
-import FreelanceIcon    from "phosphor-svelte/lib/ChartLineUp";
-import SavingIcon       from "phosphor-svelte/lib/PiggyBank";
-import InvestmentIcon   from "phosphor-svelte/lib/SketchLogo";
-import OtherIcon        from "phosphor-svelte/lib/Wallet";
-
+import {
+    CreditCard,
+    Bank,
+    Money,
+    ChartLineUp,
+    PiggyBank,
+    SketchLogo,
+    Wallet
+} from 'phosphor-svelte';
 
 export const getBalanceIcon = ( type: TypeBalance ) => ({
-    [TypeBalance.CASH]          : CashIcon,
-    [TypeBalance.BANK_ACCOUNT]  : BankIcon,
-    [TypeBalance.CREDIT]        : CreditCardIcon,
-    [TypeBalance.DEBIT]         : CreditCardIcon,
-    [TypeBalance.FREELANCE]     : FreelanceIcon,
-    [TypeBalance.SAVINGS]       : SavingIcon,
-    [TypeBalance.INVESTMENT]    : InvestmentIcon,
-    [TypeBalance.OTHER]         : OtherIcon,
-})[type] || OtherIcon;
+    [TypeBalance.CASH]          : Money,
+    [TypeBalance.BANK_ACCOUNT]  : Bank,
+    [TypeBalance.CREDIT]        : CreditCard,
+    [TypeBalance.DEBIT]         : CreditCard,
+    [TypeBalance.FREELANCE]     : ChartLineUp,
+    [TypeBalance.SAVINGS]       : PiggyBank,
+    [TypeBalance.INVESTMENT]    : SketchLogo,
+    [TypeBalance.OTHER]         : Wallet,
+})[type] || Wallet;
