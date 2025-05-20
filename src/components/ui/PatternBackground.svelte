156017e171ab4909f698c8fa0e7662f5255fd8ba
kevinkeyssx <keyss1700@gmail.com>
2025-05-20 09:36:24 -0400

    Patter updated

--- src/components/ui/PatternBackground.svelte
+++ src/components/ui/PatternBackground.svelte
@@ -5,13 +5,16 @@
     export let patternId    : string = "smallGrid"; // ID único para el patrón
 </script>
 
-<div class="absolute inset-0 opacity-5">
-    <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
-        <defs>
-            <pattern id="{patternId}" width="{gridSize}" height="{gridSize}" patternUnits="userSpaceOnUse">
-                <path d="M {gridSize} 0 L 0 0 0 {gridSize}" fill="none" stroke="{strokeColor}" stroke-width="{strokeWidth}" />
-            </pattern>
-        </defs>
-        <rect width="100%" height="100%" fill="url(#{patternId})" />
-    </svg>
+<div class="absolute inset-0 w-full h-full transition-transform duration-300 group-hover:scale-[1.02] z-0 pointer-events-none">
+    <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-white/10 to-transparent opacity-40"></div>
+    <div class="absolute inset-0 opacity-5">
+        <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+                <pattern id="{patternId}" width="{gridSize}" height="{gridSize}" patternUnits="userSpaceOnUse">
+                    <path d="M {gridSize} 0 L 0 0 0 {gridSize}" fill="none" stroke="{strokeColor}" stroke-width="{strokeWidth}" />
+                </pattern>
+            </defs>
+            <rect width="100%" height="100%" fill="url(#{patternId})" />
+        </svg>
+    </div>
 </div>
