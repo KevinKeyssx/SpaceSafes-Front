17090a8c8ddc1153f97acb884182b3ca1ab8893f
kevinkeyssx <keyss1700@gmail.com>
2025-05-20 09:42:14 -0400

    Tabs added

--- src/models/payment-services/payment-service.model.ts
+++ src/models/payment-services/payment-service.model.ts
@@ -1,14 +1,28 @@
 import type { Balance } from '@/models/balance/balance.model';
+import type { Service } from '@/models/expenses/expense.model';
+
+
+export type Navly = {
+    id      : string;
+    name    : string;
+    url     : string;
+    avatar  : string;
+    createdAt       : string;
+    updatedAt       : string;
+    category        : WebsiteCategory;
+    lastViewed      : string;
+    isFavorite      : boolean;
+}
+
 
 export type PaymentService = {
-    id: string; // UUID del pago
-    userId: string; // ID del usuario asociado
-    amount: number; // Valor monetario (puede ser entero o decimal)
-    createdAt: string; // Fecha ISO 8601
-    updatedAt: string; // Fecha ISO 8601
-    expirationDate: string; // Fecha de vencimiento
-    description: string | null; // Detalle opcional (nullable)
-    serviceId: string; // UUID del servicio relacionado
-    navlyId: string | null; // UUID opcional de Navly (nullable)
-    balance?: Balance; // ID del balance usado para pagar, opcional
+    id              : string;
+    amount          : number;
+    createdAt       : string;
+    updatedAt       : string;
+    expirationDate  : string;
+    description     : string | null;
+    service         : Service;
+    navly           : Navly | null;
+    balance?        : Balance;
 };
