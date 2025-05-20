2052475ee5c8e7b43fdf992f01824e2ddb712341
kevinkeyssx <keyss1700@gmail.com>
2025-05-20 09:36:37 -0400

    Title component added

---
+++ src/components/ui/Title.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import PatternBackground from '../ui/PatternBackground.svelte';
+</script>
+
+<header class="relative animate-slide-up flex flex-col sm:flex-row justify-between items-center p-4 bg-primary-50 dark:bg-primary-800 rounded-lg shadow border border-primary-200 dark:border-primary-700 mb-6">
+    <PatternBackground patternId="title" />
+
+    <h1 class="text-2xl font-bold mb-2 sm:mb-0 text-primary-500 dark:text-primary-400">
+        <slot name="title"/>
+    </h1>
+
+    <slot name="content"/>
+</header>
