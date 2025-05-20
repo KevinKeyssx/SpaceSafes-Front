a64e317c8884fc2729111c2abe060e89037b4c12
kevinkeyssx <keyss1700@gmail.com>
2025-05-20 09:38:19 -0400

    Line component added

---
+++ src/components/ui/Inputs/Line.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let className: string = 'my-3';
+</script>
+
+<div class="h-px w-full bg-gradient-to-r from-transparent via-primary-400/30 to-transparent {className}"></div>
