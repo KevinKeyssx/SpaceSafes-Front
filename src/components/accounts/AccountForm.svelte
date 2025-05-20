7172a746f95f69088c9a7fbd0b093b6bd55d354f
kevinkeyssx <keyss1700@gmail.com>
2025-05-20 09:26:23 -0400

    Toast added & PasswordControls implemented

--- src/components/accounts/AccountForm.svelte
+++ src/components/accounts/AccountForm.svelte
@@ -2,24 +2,36 @@
 <script lang="ts">
     import { slide } from 'svelte/transition';
 
-    import type { Account, AccountNavly } from "@/models/account/account.model";
+    import toast from 'svelte-french-toast';
+
+    import { errorToast, successToast } from '@/config/toast/toast.config';
+
     import Input            from "@/components/ui/Inputs/Input.svelte";
     import Switch           from "@/components/ui/Inputs/Switch.svelte";
-    import { addAccount, updateAccount } from '@/stores/accountsStore';
+    import OutlineButton    from '@/components/ui/Buttons/OutlineButton.svelte';
+    import type { Account, AccountNavly } from "@/models/account/account.model";
+    import ShowIcon from '@/icons/ShowIcon.svelte';
     import { loadSpaceSafes } from '@/services/fetch/getSpaceSafes';
-    import { addLink } from '@/stores/linksStore';
+    import { addLink }                      from '@/stores/linksStore';
+    import { addAccount, updateAccount }    from '@/stores/accountsStore';
+    import SaveButton from '../ui/Buttons/SaveButton.svelte';
+    import Popover from '../ui/bits/Popover.svelte';
+    import PasswordControls from '../password/PasswordControls.svelte';
+    import KeyIcon from '@/icons/KeyIcon.svelte';
 
 
     export let account: Account = {} as Account;
     export let open : boolean;
 
+
     let accountEdit = { ...account };
+    let showPassword = false;
+    let isLoadingSave = false
+
 
     if ( !accountEdit.id || accountEdit.navly?.length === 0 ) {
         accountEdit.navly = [{url: ''}];
     }
-
-    let showPassword = false;
 
 
     function isValidUrl(url: string): boolean {
@@ -35,8 +47,11 @@
         console.log('🚀 ~ file: AccountForm.svelte:12 ~ accountEdit:', accountEdit)
 
         if ( !accountEdit.username || !accountEdit.password ) {
+            toast.error( 'Debes completar los campos requeridos (*)', errorToast() );
             return;
         }
+
+        isLoadingSave = true;
 
         const { navly, ...dataWithoutNavly } = accountEdit;
         const newAccount: Record<string, any> = { ...dataWithoutNavly };
@@ -47,26 +62,30 @@
 
         console.log('🚀 ~ file: AccountForm.svelte:51 ~ newAccount:', newAccount)
 
-
         const savedAccount = await loadSpaceSafes<AccountNavly>({
             url: `/api/space-safes/accounts`,
             method: 'POST',
             data: newAccount
         });
 
-        if ( !savedAccount ) return;
+        if ( !savedAccount ) {
+            isLoadingSave = false;
+            toast.error( 'Debes completar los campos requeridos (*)', errorToast() );
+            return;
+        }
 
         addAccount( savedAccount.account );
         addLink( savedAccount.navly );
         open = false;
-
-        console.log('🚀 ~ file: AccountForm.svelte:53 ~ savedAccount:', savedAccount)
+        isLoadingSave = false;
+        toast.success( 'Cuenta registrada correctamente.', successToast() );
     }
 
 
     async function updatedAccount() {
         console.log('🚀 ~ file: AccountForm.svelte:12 ~ accountEdit:', accountEdit)
 
+        isLoadingSave = true;
 
         const  {
             createdAt,
@@ -81,14 +100,21 @@
             method: 'PATCH',
             data: rest
         });
+
         console.log('🚀 ~ file: AccountForm.svelte:80 ~ savedAccount:', savedAccount)
 
-        if ( !savedAccount ) return;
+        if ( !savedAccount ) {
+            isLoadingSave = false;
+            toast.error( 'Error al actualizar la cuenta', errorToast() );
+            return;
+        };
 
         updateAccount( savedAccount );
         open = false;
 
         console.log('🚀 ~ file: AccountForm.svelte:41 ~ newAccount:', savedAccount)
+        isLoadingSave = false;
+        toast.success( 'Cuenta actualizada correctamente.', successToast() );
     }
 
 
@@ -100,41 +126,63 @@
 
         updatedAccount();
     }
+
+    let openPassword = false
 </script>
 
 <form
-    class="p-5 space-y-1" on:submit|preventDefault={saveAccount}
+    class="px-5 py-0 space-y-3" on:submit|preventDefault={saveAccount}
     transition:slide={{ duration: 300 }}
 >
     <Input
         label="Nombre de la cuenta"
         id="name"
         type="text" 
+        placeholder="Ingrese el nombre de la cuenta"
         bind:value={accountEdit.name}
     />
 
     <Input
-        label="Nombre"
+        label="Nombre (*)"
         id="account-name"
         type="text" 
+        required
+        placeholder="Ingrese el nombre de la cuenta"
         bind:value={accountEdit.username}
     />
 
-    <div class="flex items-center gap-2"> 
+    <div class="grid grid-cols-[1fr,0.1fr,0.1fr] items-end gap-2"> 
         <Input
-            label="Contraseña"
+            label="Contraseña (*)"
             id="account-password"
             type={showPassword ? "text" : "password"} 
+            required
+            placeholder="Ingrese la contraseña"
             bind:value={accountEdit.password}
         />
 
-        <button
-            on:click={() => showPassword = !showPassword}
-            type="button"
-            aria-label="Mostrar contraseña"
+        <Popover bind:open={openPassword}>
+            {#snippet trigger()}
+                <OutlineButton
+                    onClick={() => showPassword = true}
+                >
+                    <KeyIcon />
+                </OutlineButton>
+            {/snippet}
+
+            {#snippet content()}
+                <PasswordControls
+                    bind:password={accountEdit.password}
+                    usePassword={() => openPassword = false}
+                />
+            {/snippet}
+        </Popover>
+
+        <OutlineButton
+            onClick={() => showPassword = !showPassword}
         >
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><circle cx="12" cy="12" r="0" fill="currentColor"><animate fill="freeze" attributeName="r" dur="0.2s" values="0;3"/></circle><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12c1.38 -0.77 4.42 -1.3 8 -1.3c3.58 0 6.62 0.53 8 1.3c-1.38 0.77 -4.42 1.3 -8 1.3c-3.58 0 -6.62 -0.53 -8 -1.3Z"><animate fill="freeze" attributeName="d" dur="0.5s" values="M4 12c1.38 -0.77 4.42 -1.3 8 -1.3c3.58 0 6.62 0.53 8 1.3c-1.38 0.77 -4.42 1.3 -8 1.3c-3.58 0 -6.62 -0.53 -8 -1.3Z;M2 12c1.72 -3.83 5.53 -6.5 10 -6.5c4.47 0 8.28 2.67 10 6.5c-1.72 3.83 -5.53 6.5 -10 6.5c-4.47 0 -8.28 -2.67 -10 -6.5Z"/></path></svg>
-        </button>
+            <ShowIcon />
+        </OutlineButton>
     </div>
 
     <Switch
@@ -146,10 +194,11 @@
     <span class="text-sm font-medium text-primary-700 dark:text-primary-200 mb-1">Enlaces</span>
 
     <Input
-    id="account-navly"
-    type="text"
-    bind:value={accountEdit.navly[0].url}
-/>
+        id="account-navly"
+        type="text"
+        placeholder="Ingrese el enlace"
+        bind:value={accountEdit.navly[0].url}
+    />
 
     <!-- <div bind:this={navlyContainer} class="flex flex-col gap-2 max-h-60 overflow-auto p-1">
         {#each accountEdit.navly as navly}
@@ -183,11 +232,8 @@
     >
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-dasharray="16" stroke-dashoffset="16" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M5 12h14"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.4s" values="16;0"/></path><path d="M12 5v14"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.4s" dur="0.4s" values="16;0"/></path></g></svg>
     </button> -->
-
-    <button
-        type="submit"
-        class="px-4 py-2 bg-primary-600 hover:bg-primary-700 text-white rounded-md transition-colors"
-    >
-        Guardar
-    </button>
+    <SaveButton
+        disabled={isLoadingSave}
+        isEdit={accountEdit.id ? true : false}
+    />
 </form>
